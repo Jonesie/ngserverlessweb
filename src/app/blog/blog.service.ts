c95361c35f1d364827661710437538e7c40ab0da
Jonesie <peter@jonesie.net.nz>
2017-05-13 08:35:41 +1200

    Move content to assets

--- src/app/blog/blog.service.ts
+++ src/app/blog/blog.service.ts
@@ -7,14 +7,23 @@ import 'rxjs/add/operator/catch';
 
 import { IBlogIndex } from './blogindex';
 import { IBlogPost } from './blogpost';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class BlogService {
 
-  private _blogIndexUrl: string = 'https://s3-us-west-2.amazonaws.com/jonesie.kiwi/content/blogindex.json';
-  private _blogPostUrl: string = 'https://s3-us-west-2.amazonaws.com/jonesie.kiwi/content/';
+  private _blogIndexUrl: string;
+  private _blogPostUrl: string;
 
-  constructor(private _http: Http) { }
+  constructor(private _http: Http) {
+      if(environment.production) {
+        this._blogIndexUrl = 'https://s3-us-west-2.amazonaws.com/jonesie.kiwi/assets/content/blogindex.json';
+        this._blogPostUrl = 'https://s3-us-west-2.amazonaws.com/jonesie.kiwi/assets/content/';
+      } else {
+        this._blogIndexUrl = '/assets/content/blogindex.json';
+        this._blogPostUrl = '/assets/content/';
+      }
+  }
 
   getIndex(): Observable<IBlogIndex[]> {
     return this._http.get(this._blogIndexUrl)
